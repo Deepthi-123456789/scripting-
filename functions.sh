8918a82418d53e75b2369f8dfe65feeb4b764282
Deepthi <jonnalagaddadeepthi81@gmail.com>
2024-04-30 16:57:54 +0530

    first

--- functions.sh
+++ functions.sh
@@ -3,15 +3,16 @@
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+N="\e[0m"
 echo "script name $0"
 
 validate(){
     if [ $? -ne 0 ]
     then 
-        echo -e " $R $1 is not succesfull"
+        echo -e "  $1 is $R not succesfull $N"
         exit 1
     else
-        echo -e" $g $1  is sucess"
+        echo -e"  $1  is $Gsucess $N"
     fi
 }
 
