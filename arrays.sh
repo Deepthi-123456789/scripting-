91d14ebac863b38f3bdbe4427a30634d9ea0550b
Deepthi <jonnalagaddadeepthi81@gmail.com>
2024-04-30 14:08:12 +0530

    first

--- arrays.sh
+++ arrays.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 FRUITS=("apple" "mango")
-echo "first :$(FRUITS[0])"
-echo "second :$(FRUITS[1])"
-echo "fruits :$(FRUITS[@])"
+echo "first :${FRUITS[0]}"
+echo "second :${FRUITS[1]}"
+echo "fruits :${FRUITS[@]}"
