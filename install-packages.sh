a16760e116c3ab4ef0ccde412487058d94546efe
Deepthi <jonnalagaddadeepthi81@gmail.com>
2024-04-30 21:27:31 +0530

    first

--- install-packages.sh
+++ install-packages.sh
@@ -5,6 +5,20 @@ G="\e[32m"
 N="\e[0m"
 echo "script name $0"
 
+timestamp=$(data +%F-%H-%M-%S)
+
+echo "script started excuting at $timestamp"
+
+validate(){
+    if [ $? -ne 0 ]
+    then 
+        echo -e "$1 is $R not succesfull $N"
+        exit 1
+    else
+        echo -e "$1  is $G sucess $N"
+    fi
+}
+
 if [ $ID -ne 0 ]
 then    
     echo -e "$R not a root user $N"
@@ -13,3 +27,15 @@ else
 fi
 
 echo "all the arguments passed: $@"
+
+for package in $@
+do
+    yum list installed $package
+    if [$? -ne 0]
+    then
+        yum install $package -y
+        validate "installion of  $package"
+    else 
+        echo " $package is already installed"
+    fi
+done
