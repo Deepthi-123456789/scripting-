c24130c0de7e103d7310e419f156b91a13c39bac
Deepthi <jonnalagaddadeepthi81@gmail.com>
2024-04-30 15:37:25 +0530

    first

--- install-mysql.sh
+++ install-mysql.sh
@@ -4,6 +4,15 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "not a root user"
+    exit 1
 else
     echo "root user"
+fi
+yum install mysql -y
+if [ $? -nq 0 ]
+then 
+    echo "installation is not succesfull"
+    exit 1
+else
+    echo "installing mysql is sucess"
 fi
