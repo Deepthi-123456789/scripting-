08c84d7063d060bd81c4a783a969a118fae336d2
Deepthi <jonnalagaddadeepthi81@gmail.com>
2024-04-30 15:58:33 +0530

    first

---
+++ functions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ID=$(id -u)
+validate(){
+    if [ $? -ne 0 ]
+    then 
+        echo "installation is not succesfull"
+        exit 1
+    else
+        echo "installing  is sucess"
+    fi
+}
+if [ $ID -ne 0 ]
+then
+    echo "not a root user"
+    exit 1
+else
+    echo "root user"
+fi
+yum install mysql -y
+validate
+yum install git -y
+validate
