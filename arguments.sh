240fc02c27963de49a594bedc264f6637cfea411
Deepthi <jonnalagaddadeepthi81@gmail.com>
2024-04-30 16:36:40 +0530

    first

--- arguments.sh
+++ arguments.sh
@@ -6,3 +6,8 @@ echo "$number1: hi, $number2"
 echo "$number2: i am fine $number1"
 echo "$number1: How are u $number2"
 echo "$number2: fine"
+
+echo "exit status : $?"
+echo "name: $0"
+echo "no.of all arguments: $@"
+echo "how many arg passed: $#"
--- arrays.sh
+++ arrays.sh
@@ -3,3 +3,5 @@ FRUITS=("apple" "mango")
 echo "first :${FRUITS[0]}"
 echo "second :${FRUITS[1]}"
 echo "fruits :${FRUITS[@]}"
+
+
