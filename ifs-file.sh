9b2a4e6abc730ea021c6d65ab9607a3941f2694a
Deepthi <jonnalagaddadeepthi81@gmail.com>
2024-05-03 21:24:08 +0530

    first

---
+++ ifs-file.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source_dir="/tmp/scripting-logs"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+
+
+if [ ! -d $source_dir ]
+then
+    echo -e "source dir does not exists"
+fi
+
+
+whil IFS=":" read -r username password user-id group-id user-fullname home-dir shell-path
+do 
+    echo "username =$username"
+    echo "password =$password"
+    echo "user_id =$user_id"
+done < $file
