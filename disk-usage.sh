fc7eccb826ae5b334f26fca0ee3f705206760a1c
Deepthi <jonnalagaddadeepthi81@gmail.com>
2024-05-03 21:51:17 +0530

    first

--- disk-usage.sh
+++ disk-usage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Disk_usage=$(df -hT |grep -vE 'tmp/file')
+Disk_usage=$(df -hT |grep -vE 'tmp|file')
 Disk_threshold=1
 message=""
 
@@ -10,7 +10,7 @@ do
     partion=$(echo $line | awk '{prink $6}')
     if [ $usage -ge $Disk_threshold ]
     then
-        message+="high disk usage\n"
+        message+="high disk usage on $partion : $usage\n"
     fi
 done <<< $Disk_usage
 echo -e "message: $meaasage"
