5f7975592391bd28858c91e67612557d914b8467
Deepthi <jonnalagaddadeepthi81@gmail.com>
2024-05-04 12:23:43 +0530

    first

--- greetings.sh
+++ greetings.sh
@@ -3,8 +3,8 @@
 Name=""
 Wishes=""
 
-Usage(){
-    echo "Usage:: $(basename $0) -n <name> -w <wishes>"
+USAGE(){
+    echo "USAGE:: $(basename $0) -n <name> -w <wishes>"
     echo "options::"
     echo "-n, specify the name"
     echo "-w, specify the wishes.(optinal). default is good morning"
@@ -15,15 +15,15 @@ while getopts ":n:w:h" opt; do
     case $opt in
         n)Name="$optarg";;
         w)Wishes="$optarg";;
-        ?)echo "invalid options: -"$optarg"" >&2; Usage; exit;;
-        :)Usage; exit;;
-        h)Usage; exit;;
+        ?)echo "invalid options: -"$optarg"" >&2; USAGE; exit;;
+        :)USAGE; exit;;
+        h)USAGE; exit;;
     esac
 done
 
-if [ -z "$Name" ] || [ -z "$Wishes" ]; then
+if [ -z "$Name" ]; then
     echo "Error: -n is mandarotry optins."
-    Usage
+    USAGE
     exit 1
 fi
 
